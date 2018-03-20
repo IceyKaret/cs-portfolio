2e56d01760b0adc6049bea0e2670137dcdbc1f20
EvilKaret <epang.sh@mysummitps.org>
2018-03-20 17:18:48 +0000

    first commit

---
+++ int3checkpoints/info.js
@@ -0,0 +1,125 @@
+//Creates variables which values correspond to their IDs
+
+var one = document.getElementById("one")
+var two = document.getElementById("two")
+var three = document.getElementById("three")
+var four = document.getElementById("four")
+var five = document.getElementById("five")
+var six = document.getElementById("six")
+var seven = document.getElementById("seven")
+var eight = document.getElementById("eight")
+var nine = document.getElementById("nine")
+var ten = document.getElementById("ten")
+var eleven = document.getElementById("eleven")
+var twelve = document.getElementById("twelve")
+var thirteen = document.getElementById("thirteen")
+var fourteen = document.getElementById("fourteen")
+var fifteen = document.getElementById("fifteen")
+var sixteen = document.getElementById("sixteen")
+var seventeen = document.getElementById("seventeen")
+var eighteen = document.getElementById("eighteen")
+var nineteen = document.getElementById("nineteen")
+var twenty = document.getElementById("twenty")
+var twentyone = document.getElementById("twentyone")
+var twentytwo = document.getElementById("twentytwo")
+var twentythree = document.getElementById("twentythree")
+var twentyfour = document.getElementById("twentyfour")
+var display = document.getElementById("display")
+
+//Runs the code block when the element is clicked.
+
+one.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Try again"
+});
+
+two.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Haha"
+});
+
+three.addEventListener("click", function(){
+    display.innerHTML = "You Lose, HAHA"
+});
+
+four.addEventListener("click", function(){
+    display.innerHTML = "You Lose, MUAHAHAHA"
+});
+
+five.addEventListener("click", function(){
+    display.innerHTML = "You Lose, lol"
+});
+
+six.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Different block"
+});
+
+seven.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Whats your question?"
+});
+
+eight.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Are you looking for something?"
+});
+
+nine.addEventListener("click", function(){
+    display.innerHTML = "You Lose, There is nothing here"
+});
+
+ten.addEventListener("click", function(){
+    display.innerHTML = "You Lose, what are you doing?"
+});
+
+eleven.addEventListener("click", function(){
+    display.innerHTML = "You Lose";
+});
+
+twelve.addEventListener("click", function(){
+    display.innerHTML = "You Lose, this is boring isn't it?"
+});
+
+thirteen.addEventListener("click", function(){
+    display.innerHTML = "You Lose..."
+});
+
+fourteen.addEventListener("click", function(){
+    display.innerHTML = "You Lose again, and again, and again, and again"
+});
+
+fifteen.addEventListener("click", function(){
+    display.innerHTML = "You Lose"
+});
+
+sixteen.addEventListener("click", function(){
+    display.innerHTML = "You Lose, haha"
+});
+
+seventeen.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Try Harder"
+});
+
+eighteen.addEventListener("click", function(){
+    display.innerHTML = "You Lose~"
+});
+
+nineteen.addEventListener("click", function(){
+    display.innerHTML = "You Lose, everything you ever wanted"
+});
+
+twenty.addEventListener("click", function(){
+    display.innerHTML = "You Lose, this game"
+});
+
+twentyone.addEventListener("click", function(){
+    display.innerHTML = "You Lose, again"
+});
+
+twentytwo.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Please stop already"
+});
+
+twentythree.addEventListener("click", function(){
+    display.innerHTML = "You Lose, you pressed the red button"
+});
+
+twentyfour.addEventListener("click", function(){
+    display.innerHTML = "You Lose, Must have missed one";
+});
